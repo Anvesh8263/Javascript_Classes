6c611e419a8436600d524b307bd86f8cb1c97154
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-06-22 13:25:43 +0530

    Create Lecture-1.js

---
+++ Lecture-1.js
@@ -0,0 +1,30 @@
+What are the Datatypes in Javascript
+
+String
+Number
+Boolean
+Undefined
+Null
+BigInt
+Symbol
+
+What is String?
+A string is a sequence of one or more characters that may consist of letters, numbers, or symbols.
+
+what is Number?
+In JavaScript, numbers are primitive data types. For example, const a = 3; const b = 3.13;
+
+What is Boolean? 
+The Javascript standard defines true and false values as a unique data type called a Javascript boolean. It may be call true or false
+
+What is Undefined?
+A variable that has not been assigned a value is of type undefined .
+
+What is Null?
+The null value represents the intentional absence of any object value.
+
+What is BigInt?
+A BigInt value, also sometimes just called a BigInt, is a bigint primitive, created by appending n to the end of an integer literal, or by calling the BigInt() function (without the new operator) and giving it an integer value or string value.
+
+What is Symbol?
+Symbol is a built-in object whose constructor returns a symbol primitive — also called a Symbol value or just a Symbol — that's guaranteed to be unique.
