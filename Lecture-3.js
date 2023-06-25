98141bbff681f7f2e7ac6f7c53341b3e505056ca
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-06-25 09:39:51 +0530

    Create Lecture-3.js

---
+++ Lecture-3.js
@@ -0,0 +1,32 @@
+let score =30
+let to=score==30
+console.log(to)
+//== vs ===
+console.log('5'+5)
+console.log('5'-5)
+console.log('5'-'5')
+console.log(5- -'5')
+console.log('5'==5)
+console.log('5'===5)
+console.log(0==false)
+
+//decision making 
+// if-else
+let istrue=true;
+istrue?console.log("Hello"):console.log("Hii")
+
+function
+
+function add(name){
+    console.log('My Name is' +name);
+    console.log(`My Name is ${name}`); //important
+ }
+
+add('Anvesh')
+object
+let obj={
+ name:'iphone',
+ price: 1300000,
+ isBuy:true
+}
+console.log(obj.price);
